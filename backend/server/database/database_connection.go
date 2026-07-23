e772cdeda359c02484bd0560a9917211bfce802f
keverrettcode-dev <keverrett.code@gmail.com>
2026-07-23 12:44:51 -0400

    Setting up DB connection/.env

--- backend/server/database/database_connection.go
+++ backend/server/database/database_connection.go
@@ -1,1 +1,16 @@
 package database
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+func DBInstance() *mongo.Client{ 
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Println("Warning: unable to find .env file!")
+	}
+ }
