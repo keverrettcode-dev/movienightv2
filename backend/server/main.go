04f2aa7696bdc5ae0cefee204da8cd4b27883d92
keverrettcode-dev <keverrett.code@gmail.com>
2026-07-23 12:11:34 -0400

    Setting up routes/controllers

--- backend/server/main.go
+++ backend/server/main.go
@@ -4,15 +4,20 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+
+	controller "github.com/keverrettcode-dev/movienightv2/backend/server/controllers"
 )
 
 func main() {
 	//This is the main function
 
 	router:=gin.Default()
+
 	router.GET("/slayer", func(c *gin.Context) {
 		c.String(200, "Hello, from the backend, Sr. Developer Everrett!")
 	})
+
+	router.GET("/movies", controller.GetMovies())
 
 	if err:=router.Run(":8080"); err != nil{
 		fmt.Println("Failed to start server sir!", err)
