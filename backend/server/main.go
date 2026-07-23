eb5324fcbbac657a62905dc1f918d605d4c851b1
keverrettcode-dev <keverrett.code@gmail.com>
2026-07-23 11:21:17 -0400

    Go Http Server Running Successfully

--- backend/server/main.go
+++ backend/server/main.go
@@ -1,1 +1,20 @@
-package server
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	//This is the main function
+
+	router:=gin.Default()
+	router.GET("/slayer", func(c *gin.Context) {
+		c.String(200, "Hello, from the backend, Sr. Developer Everrett!")
+	})
+
+	if err:=router.Run(":8080"); err != nil{
+		fmt.Println("Failed to start server sir!", err)
+	}
+}
