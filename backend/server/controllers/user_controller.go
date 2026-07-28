189313430aa3fe0826e6a1df9b7ddc9005eaff9a
keverrettcode-dev <keverrett.code@gmail.com>
2026-07-28 11:04:27 -0400

    Jwt Auth Setup

--- backend/server/controllers/user_controller.go
+++ backend/server/controllers/user_controller.go
@@ -3,10 +3,12 @@ package controllers
 import (
 	"context"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	jwt "github.com/golang-jwt/jwt/v5"
 	"github.com/keverrettcode-dev/movienightv2/backend/server/database"
 	"github.com/keverrettcode-dev/movienightv2/backend/server/models"
 	"go.mongodb.org/mongo-driver/v2/bson"
@@ -73,4 +75,82 @@ func RegisterUser() gin.HandlerFunc{
 
 
 	}
+}
+
+func UserLogin() gin.HandlerFunc{
+	return func(c *gin.Context) {
+		var userLogin models.UserLogin
+		if err := c.ShouldBindJSON(&userLogin); err != nil{
+			c.JSON(http.StatusBadRequest, gin.H{"error":"Invalid input data"})
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var foundUser models.User
+
+		err := userCollection.FindOne(ctx, bson.M{"email": userLogin.Email}).Decode(&foundUser)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or password"})
+			return
+		}
+
+		err = bcrypt.CompareHashAndPassword([]byte(foundUser.Password), []byte(userLogin.Password))
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or password"})
+			return
+		}
+	}
+}
+
+type SignedDetails struct {
+	Email string
+	FirstName string
+	LastName string
+	Role string
+	UserId string
+	jwt.RegisteredClaims
+}
+var SECRET_KEY string = os.Getenv("SECRET_KEY")
+
+func GenerateAllTokens(email, firstName, lastName, role, userId string)(string, string, error) {
+	claims := &SignedDetails{
+		Email: email,
+		FirstName: firstName,
+		LastName: lastName,
+		Role: role,
+		UserId: userId,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer: "MagicStream",
+			IssuedAt: jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate((time.Now().Add(24 * time.Hour))),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		return "", "", err
+	}
+
+	refreshedClaims := &SignedDetails{
+		Email: email,
+		FirstName: firstName,
+		LastName: lastName,
+		Role: role,
+		UserId: userId,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer: "MagicStream",
+			IssuedAt: jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate((time.Now().Add(24 * time.Hour))),
+		},
+	}
+
+	refreshToken := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshedClaims)
+	signedRefreshToken, err := refreshToken.SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		return "", "", err
+	}
+
+	return signedToken, signedRefreshToken, nil
 }
