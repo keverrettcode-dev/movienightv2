49d2105b1a40783ee95cf98f22d4e4eb040da260
keverrettcode-dev <keverrett.code@gmail.com>
2026-07-28 10:28:34 -0400

    UserLogin Function

--- backend/server/models/user_model.go
+++ backend/server/models/user_model.go
@@ -22,3 +22,17 @@ type User struct {
 	RefreshToken string `json:"refresh_token" bson:"refresh_token"`
 	FavoriteGenres []Genre `json:"favourite_genres" bson:"favourite_genres" validate:"required,dive"`
 }
+
+type UserLogin struct {
+	Email string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=6"`
+}
+
+type UserResponse struct {
+	UserId string `json:"user_id"`
+	FirstName string `json:"first_name"`
+	Lastname string `json:"last_name"`
+	Email string `json:"email"`
+	Role string `json:"role"`
+	FavouriteGenres []Genre `json:"favourite_genres"`
+}
